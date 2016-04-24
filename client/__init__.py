f31cf98368b18598311490cd39f935282d025004
Jon Robison <narfman0@gmail.com>
2016-04-24 02:52:25 -0400

    -Add pypi support (setup)

---
+++ client/__init__.py
@@ -0,0 +1,5 @@
+__description__ = """Jasper is an open source platform for developing always-on,
+        voice-controlled applications.
+        """
+__title__ = 'Jasper'
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,42 @@
+import functools
+import pkg_resources
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements as parse_reqs
+
+import client as jasper
+
+
+# Compatibility with older versions of pip
+pip_dist = pkg_resources.get_distribution('pip')
+pip_version = tuple(map(int, pip_dist.version.split('.')))
+
+# Use a base partial that will be updated depending on the version of pip
+parse_requirements = functools.partial(parse_reqs, options=None)
+
+if pip_version >= (1, 5):
+    # pip 1.5 introduced a session kwarg that is required in later versions
+    from pip.download import PipSession
+    parse_requirements.keywords['session'] = PipSession()
+
+
+setup(name=jasper.__title__,
+      version=jasper.__version__,
+      description=jasper.__description__,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='jasper tts stt',
+      url='https://github.com/jasperproject/jasper-client',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          str(req.req) for req in parse_requirements('client/requirements.txt')
+      ],
+)
